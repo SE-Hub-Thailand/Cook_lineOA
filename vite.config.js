c2a38d5854dfb1f701f749f08851ceaf79a55f9f
Mabel <mobile44.mm@gmail.com>
2024-10-02 12:11:24 +0700

    update ite.config.js to http

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [react()],
-//   })
-
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import fs from 'fs'
-import path from 'path'
-
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem'))
-    },
-    host: true,  // หรือใช้ '--host' ก็ได้
-    port: 5173,  // คุณสามารถเปลี่ยน port ได้ถ้าต้องการ
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+  })
+
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import fs from 'fs'
+// import path from 'path'
+
+// export default defineConfig({
+//   server: {
+//     https: {
+//       key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+//       cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem'))
+//     },
+//     host: true,  // หรือใช้ '--host' ก็ได้
+//     port: 5173,  // คุณสามารถเปลี่ยน port ได้ถ้าต้องการ
+//   },
+//   plugins: [react()],
+// })
