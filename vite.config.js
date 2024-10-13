75bc2a89d9032864f47c21cbe0b135f72b4c6bb1
Mabel <mobile44.mm@gmail.com>
2024-10-13 07:56:38 +0700

    https

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-  })
-
-// import react from '@vitejs/plugin-react'
-// import { defineConfig } from 'vite'
-// import fs from 'fs'
-// import path from 'path'
-
 // export default defineConfig({
-//   server: {
-//     https: {
-//       key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
-//       cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem'))
-//     },
-//     host: true,  // หรือใช้ '--host' ก็ได้
-//     port: 5173,  // คุณสามารถเปลี่ยน port ได้ถ้าต้องการ
-//   },
+//     plugins: [react()],
+//   })
+// export default defineConfig({
 //   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://cookbstaging.careervio.com',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//       }
+//     }
+//   }
 // })
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import fs from 'fs'
+import path from 'path'
+
+export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem'))
+    },
+    host: true,  // หรือใช้ '--host' ก็ได้
+    port: 5173,  // คุณสามารถเปลี่ยน port ได้ถ้าต้องการ
+  },
+  plugins: [react()],
+})
