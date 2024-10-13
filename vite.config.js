f90f3620ffcbce9ab86e5ba8c04bdd46b8ef7248
Mabel <mobile44.mm@gmail.com>
2024-10-13 08:03:37 +0700

    http update1

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [react()],
-//   })
+export default defineConfig({
+    plugins: [react()],
+  })
+
 // export default defineConfig({
 //   plugins: [react()],
 //   server: {
@@ -17,19 +18,19 @@
 //   }
 // })
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import fs from 'fs'
-import path from 'path'
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import fs from 'fs'
+// import path from 'path'
 
-export default defineConfig({
-  server: {
-    https: {
-      key: '/root/Cook_lineOA/key.pem',
-      cert: '/root/Cook_lineOA/cert.pem',
-    },
-    host: true,  // หรือใช้ '--host' ก็ได้
-    port: 5173,  // คุณสามารถเปลี่ยน port ได้ถ้าต้องการ
-  },
-  plugins: [react()],
-})
+// export default defineConfig({
+//   server: {
+//     https: {
+//       key: '/root/Cook_lineOA/key.pem',
+//       cert: '/root/Cook_lineOA/cert.pem',
+//     },
+//     host: true,  // หรือใช้ '--host' ก็ได้
+//     port: 5173,  // คุณสามารถเปลี่ยน port ได้ถ้าต้องการ
+//   },
+//   plugins: [react()],
+// })
