b21eb7085220e6d561ce606a2743a49886f6a13c
Mabelzzz <mobile44.mm@gmail.com>
2025-01-29 14:53:04 +0700

    add vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
-export default defineConfig({
-  // server: {
-  //   port: 3000, // กำหนดพอร์ตที่ต้องการ
-  // },
-    plugins: [react()],
-  })
-
 // export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://cookbstaging.careervio.com',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, '')
-//       }
-//     }
-//   }
-// })
+//   // server: {
+//   //   port: 3000, // กำหนดพอร์ตที่ต้องการ
+//   // },
+//     plugins: [react()],
+//   })
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://cookkeptback.sehub-thailand.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
 
 // import react from '@vitejs/plugin-react'
 // import { defineConfig } from 'vite'
